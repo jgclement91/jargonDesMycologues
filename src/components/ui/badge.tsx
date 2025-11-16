1c79fa5c8f25bf6a899a8d1f870f9326a0d75da8
jgclement91 <8974093+jgclement91@users.noreply.github.com>
2025-11-16 17:11:13 -0500

    fixes 2

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -25,22 +25,23 @@ const badgeVariants = cva(
   }
 )
 
-function Badge({
-  className,
-  variant,
-  asChild = false,
-  ...props
-}: React.ComponentProps<"span"> &
-  VariantProps<typeof badgeVariants> & { asChild?: boolean }) {
+const Badge = React.forwardRef<
+  HTMLSpanElement,
+  React.ComponentProps<"span"> &
+    VariantProps<typeof badgeVariants> & { asChild?: boolean }
+>(({ className, variant, asChild = false, ...props }, ref) => {
   const Comp = asChild ? Slot : "span"
 
   return (
     <Comp
+      ref={ref as any}
       data-slot="badge"
       className={cn(badgeVariants({ variant }), className)}
       {...props}
     />
   )
-}
+})
+
+Badge.displayName = "Badge"
 
 export { Badge, badgeVariants }
